95ee1842843dea87ab3234d6ce9a0ab52eb4618c
CaosMen <bruno.lemos.ec@gmail.com>
2020-06-12 17:10:19 -0300

    Update Thanks.jsx

--- src/views/Terms/Thanks.jsx
+++ src/views/Terms/Thanks.jsx
@@ -14,39 +14,57 @@ class Thanks extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          redirect: false
+            last_id: null,
+            redirect: false
         }
     }
 
     componentDidMount() {
         if(sessionStorage.getItem('accepted')&&sessionStorage.getItem('participant')&&sessionStorage.getItem('survey1')&&sessionStorage.getItem('survey2')&&sessionStorage.getItem('survey3')) {
-            let participant = JSON.parse(sessionStorage.getItem('participant'));
+        let participant_info = JSON.parse(sessionStorage.getItem('participant'));
+        axios.post(baseUrl + '/createParticipant', participant_info)
+            .then((res) => {
+                this.setState(prevState => ({
+                    ...prevState, last_id: res.data.id
+                }), () => {
+                    let participant = JSON.parse(sessionStorage.getItem('participant'));
+        
+                    axios.post(baseUrl + '/createParticipant', participant)
+                    .then( resp => 
+                    {
+                        let i, req = [];
+                        for(i = 1; i <= 3; i++)
+                        {
+                            let survey = JSON.parse(sessionStorage.getItem('survey' + i));
+                            survey.participant_id = this.state.last_id;
+                            let promise = axios({
+                                method: 'post',
+                                url: baseUrl+'/createSurvey',
+                                data: survey
+                            });
+                            req.push(promise);
+                        }
+                        axios.all(req)
+                        .then(axios.spread((...responses)=>{
+                            responses.forEach(res=> console.log('Saved'));
 
-            axios.post(baseUrl + '/createParticipant', participant)
-            .then( resp => 
-            {
-                let i, req = [];
-                for(i = 1; i <= 3; i++)
-                {
-                    let survey = JSON.parse(sessionStorage.getItem('survey' + i));
-                    let promise = axios({
-                        method: 'post',
-                        url: baseUrl+'/createSurvey',
-                        data: survey
+                            sessionStorage.clear();
+                        }))
+                        .catch(error=>{
+                            console.log(error);
+                        })
                     })
-                    req.push(promise);
-                }
-                axios.all(req)
-                .then(axios.spread((...responses)=>{
-                    responses.forEach(res=> console.log('Saved'));
-
-                    sessionStorage.clear();
-                }))
-                .catch(error=>{
-                    console.log(error);
-                })
+                    .catch(error=> console.log(error));
+                });
             })
-            .catch(error=> console.log(error));
+            .catch(error => {
+                //alert("Error server " + error);
+                if (axios.isCancel(error)) {
+                    console.log('Request canceled');
+                } else {
+                    console.log("Error server " + error);
+                }
+            });
         } else {
             this.redirect();
         }
