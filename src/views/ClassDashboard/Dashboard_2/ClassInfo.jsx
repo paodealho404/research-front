9e958849a6cb7436c027de6766ad8afc63631874
CaosMen <bruno.lemos.ec@gmail.com>
2020-06-15 16:58:39 -0300

    Mudanças
    - Adicionar o gráfico Progresso de Interação da Turma com os Recursos no Dashboard 2, mas com o título trocado para: Impacto das Missões na Interação da Turma com os Recursos.
    - Remoção de import não usado.

--- src/views/ClassDashboard/Dashboard_2/ClassInfo.jsx
+++ src/views/ClassDashboard/Dashboard_2/ClassInfo.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
   Row,
+  ButtonGroup,
   Col
 } from "reactstrap";
 
@@ -11,14 +17,15 @@ import Dropdown from "../../../components/Dropdown/Dropdown.jsx";
 import StudentsRegistered from "../DescripitiveData/StudentsRegistered";
 import AverageStudentsPoints from "../DescripitiveData/AverageStudentsPoints";
 import MinimumPercentage from "../DescripitiveData/MinimumPercentage";
+import HelpIcon from "../../../components/HelpIcon/HelpIcon.jsx";
 
 import { LevelChart, LevelChartChild } from "../Chart/LevelChart/LevelChart.jsx";
-import { ReachStatisticsChart, ReachStatisticsChild } from "../Chart/ReachStatisticsChart/ReachStatisticsChart.jsx";
+import { PerformanceChart } from "../Chart/PerformanceChart/PerformanceChart.jsx";
 import { MissionChart, MissionChartChild } from "../Chart/MissionChart/MissionChart.jsx";
 
 class ClassInfo extends React.Component{ 
 render() {
-  const { courseId, classroomId, curriculumId } = this.props;
+  const { courseId, classroomId, curriculumId, optionSelected } = this.props;
   return(
     <div>
       <Row>
@@ -32,6 +39,59 @@ render() {
           <MinimumPercentage classroomId={ classroomId } curriculumId={ curriculumId }/>
         </Col>
         <Col md="12">
+          <Card tag = "h5">
+            <CardHeader>
+              <CardTitle>
+                <div className = "d-flex justify-content-end mx-3"> 
+                  <div className = "mr-auto p-2">
+                    <div className="text-center">Impacto das Missões na Interação da Turma com os Recursos.</div>
+                  </div>
+                  <div className = "p-2">
+                      <ButtonGroup>
+                          <Col>
+                            <HelpIcon id = "1" icon = "fa fa-info-circle" 
+                            help = {
+                              <div className="text-center">
+                                Este gráfico mostra o progresso 
+                                da turma com o passar do tempo 
+                                e o impacto das missões criadas na 
+                                porcentagem da interação da 
+                                turma com os recursos.
+                              </div>
+                            }/>
+                          </Col>
+                          <Col>
+                            { optionSelected === "Dinâmicas" ? (
+                                <HelpIcon id = "2" icon = "fa fa-exclamation-triangle" color="red"
+                                help = {
+                                  <div className="text-center">
+                                    Está faltando 2 dias para para o período previsto para o domínio deste assunto finalizar e ainda 50% dos alunos não alcançaram a porcentagem mínima de interação com os recursos. Você pode criar uma missão para motivar esses alunos.
+                                  </div>
+                                }/>
+                              ) : (
+                                <HelpIcon id = "2" icon = "fa fa-exclamation-triangle"
+                                help = {
+                                  <div className="text-center">
+                                    Nenhum alerta para este assunto.
+                                  </div>
+                                }/>
+                              )
+                            }
+                          </Col>
+                      </ButtonGroup>
+                  </div>  
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardBody>
+              <div style = {{ borderTop: "0.1em solid grey", opacity: "0.3", margin: "0 0.5em 1em 0.5em" }}></div>
+                <div>
+                  <PerformanceChart classroomId={ classroomId } curriculumId={ curriculumId } courseId={ courseId }/>
+                </div>
+            </CardBody>
+            <CardFooter>
+            </CardFooter>
+          </Card>
         </Col>
         <Col md="12">
           <Dropdown 
