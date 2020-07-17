59188b0840acb91a247581b25c67c8f02f001aa8
CaosMen <bruno.lemos.ec@gmail.com>
2020-07-17 20:56:20 -0300

    Mudanças no envio dos dados
    - Os dados são enviados no momento da submissão.

--- src/views/Terms/FavoriteSurvey.jsx
+++ src/views/Terms/FavoriteSurvey.jsx
@@ -10,9 +10,15 @@ import {
 
 import Header from "./components/Header";
 
+import axios from 'axios';
+
+import Auth from "../Auth";
+
 import Dashboard1 from '../../assets/img/Dashboard_1.png';
 import Dashboard2 from '../../assets/img/Dashboard_2.png';
 import Dashboard3 from '../../assets/img/Dashboard_3.png';
+
+const baseUrl = ((process.env.REACT_APP_API_URL) || "http://localhost:4000") + '/course';
 
 class FavoriteSurvey extends React.Component{
     constructor(props)
@@ -21,22 +27,22 @@ class FavoriteSurvey extends React.Component{
         this.state={
             dashboard: '',
             redirect: false,
+            redirect_bd: false,
             formValid: false,
             submit: false,
             formErrors: '',
-        }
+            last_id: null,
+        };
         
         this.handleChange = this.handleChange.bind(this);
     }
 
-    componentDidMount()
-    {
-        if(!(sessionStorage.getItem('survey1')&&sessionStorage.getItem('survey2')&&sessionStorage.getItem('survey3')))
-        {
+    componentDidMount() {
+        if (!(Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey1'))) && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey2'))) && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey3'))))) {
             this.redirect();
         }
     }
-
+    
     handleChange(event) {
         let value = event.target.value;
         this.setState({ dashboard: value });
@@ -46,15 +52,55 @@ class FavoriteSurvey extends React.Component{
     submit() {
         let best_dashboard = parseInt(this.state.dashboard);
 
-        let participant = JSON.parse(sessionStorage.getItem('participant'));
+        let participant_info = JSON.parse(sessionStorage.getItem('participant'));
 
-        participant.favorite_dashboard = best_dashboard;
+        participant_info.favorite_dashboard = best_dashboard;
 
-        sessionStorage.setItem('participant', JSON.stringify(participant));
+        sessionStorage.setItem('participant', JSON.stringify(participant_info));
 
-        this.setState({
-            submit: true
-        });
+        if(Auth.authenticate_participant(participant_info) && sessionStorage.getItem('accepted') && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey1'))) && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey2'))) && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey3')))) {
+            axios.post(baseUrl + '/createParticipant', participant_info)
+                .then((res) => {
+                    this.setState(prevState => ({
+                        ...prevState, last_id: res.data.id
+                    }), () => {
+                        let i, req = [];
+                        for(i = 1; i <= 3; i++)
+                        {
+                            let survey = JSON.parse(sessionStorage.getItem('survey' + i));
+                            survey.participant_id = this.state.last_id;
+                            let promise = axios({
+                                method: 'post',
+                                url: baseUrl+'/createSurvey',
+                                data: survey
+                            });
+                            req.push(promise);
+                        }
+                        axios.all(req)
+                            .then(axios.spread((...responses)=>{
+                                responses.forEach(res => {});
+        
+                                this.setState({
+                                    submit: true
+                                });
+                            }))
+                            .catch(error=>{
+                                console.log(error);
+                            });
+                    });
+                })
+                .catch(error => {
+                    //alert("Error server " + error);
+                    if (axios.isCancel(error)) {
+                        //console.log('Request canceled');
+                    } else {
+                        console.log("Error server " + error);
+                        this.redirect_bd();
+                    }
+                });
+            } else {
+                this.redirect();
+            }
     }
 
     redirect(){
@@ -63,9 +109,16 @@ class FavoriteSurvey extends React.Component{
         });
     }
 
+    redirect_bd(){
+        this.setState({
+            redirect_bd: true
+        });
+    }
+
     render() {
         let participant_info = JSON.parse(sessionStorage.getItem('participant'));
-        return( (sessionStorage.getItem('accepted') && sessionStorage.getItem('participant')) ? (
+        
+        return( (sessionStorage.getItem('participant') && sessionStorage.getItem('accepted')) ? (
             <div>
                 <Header/>
                 <Col>
@@ -108,8 +161,8 @@ class FavoriteSurvey extends React.Component{
                                     <input className="text-secondary" style={{marginRight: '0.75em', transform: 'scale(1.5)', verticalAlign: 'middle', position: 'relative'}} id="3" value="3" name="dashboard-select" type="radio" />
                                     <div style={{fontSize: '2em', display: 'inline', verticalAlign: 'middle', position: 'relative'}}>Painel - Interação com os Recursos de Aprendizagem e Elementos de Jogos</div>
                                 </label>
-                        </div></
-                        div>
+                            </div>
+                        </div>
                         <br/>
                         <br/>
                         <Col>
@@ -120,6 +173,7 @@ class FavoriteSurvey extends React.Component{
                         </Col>
                     </Form>
                     {this.state.redirect ? (<Redirect to={{pathname:"/admin/classDashboard_" + participant_info.dashboard_sequence[0] + "/7/345/32"}}/>) : (<div></div>)}
+                    {this.state.redirect_bd ? (<Redirect to={{ pathname:"/", state: {message: 'Perdão, houve um problema durante o envio dar informações ao banco. Tente novamente mais tarde.'}}}/>) : <div></div>}
                 </Col>
             </div>
             ) : (
@@ -128,4 +182,5 @@ class FavoriteSurvey extends React.Component{
         )
     }
 }
+
 export default FavoriteSurvey;
--- src/views/Terms/Thanks.jsx
+++ src/views/Terms/Thanks.jsx
@@ -2,82 +2,55 @@ import React from 'react';
 
 import { Redirect } from 'react-router-dom';
 
-import axios from 'axios';
-
 import Header from "./components/Header";
 
 import Auth from "../Auth";
-
-const baseUrl = ((process.env.REACT_APP_API_URL) || "http://localhost:4000") + '/course';
 
 class Thanks extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             last_id: null,
-            redirect: false
-        }
+            redirect: false,
+            redirect_start: false
+        };
     }
 
     componentDidMount() {
-        if(Auth.authenticate_participant(JSON.parse(sessionStorage.getItem('participant'))) && sessionStorage.getItem('accepted') && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey1'))) && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey2'))) && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey3')))) {
-        let participant_info = JSON.parse(sessionStorage.getItem('participant'));
-        axios.post(baseUrl + '/createParticipant', participant_info)
-            .then((res) => {
-                this.setState(prevState => ({
-                    ...prevState, last_id: res.data.id
-                }), () => {
-                    let i, req = [];
-                    for(i = 1; i <= 3; i++)
-                    {
-                        let survey = JSON.parse(sessionStorage.getItem('survey' + i));
-                        survey.participant_id = this.state.last_id;
-                        let promise = axios({
-                            method: 'post',
-                            url: baseUrl+'/createSurvey',
-                            data: survey
-                        });
-                        req.push(promise);
-                    }
-                    axios.all(req)
-                    .then(axios.spread((...responses)=>{
-                        responses.forEach(res=> console.log('Saved'));
-
-                        sessionStorage.clear();
-                    }))
-                    .catch(error=>{
-                        console.log(error);
-                    })
-                });
-            })
-            .catch(error => {
-                //alert("Error server " + error);
-                if (axios.isCancel(error)) {
-                    //console.log('Request canceled');
-                } else {
-                    console.log("Error server " + error);
-                }
-            });
-        } else {
+        if (!(Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey1'))) && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey2'))) && Auth.authenticate_survey(JSON.parse(sessionStorage.getItem('survey3'))))) {
             this.redirect();
+        } else {
+            sessionStorage.clear();
+
+            setTimeout(function() {
+                this.setState({
+                    redirect_start: true
+                });
+            }.bind(this), 5000);
         }
     }
 
-    redirect() {
+    redirect(){
         this.setState({
             redirect: true
         });
     }
 
     render() {
-        return(
-            <div>
-                <Header/>
-                <center className="col" style={{height: '585px', flexDirection: 'column', justifyContent: 'center', display: 'flex'}}>
-                    <p style={{color: "#000000", fontSize: '22px'}}>Este é o final da pesquisa. Muito obrigado pela colaboração!</p>
-                    {this.state.redirect ? (<Redirect to={{ pathname:"/", state: {message: 'Perdão, houve um problema durante o envio dar informações ao banco. Tente novamente mais tarde.'}}}/>) : <div></div>}
-                </center>
-            </div>
+        let participant_info = JSON.parse(sessionStorage.getItem('participant'));
+        
+        return( (sessionStorage.getItem('participant') && sessionStorage.getItem('accepted')) ? (
+                <div>
+                    <Header/>
+                    <center className="col" style={{height: '585px', flexDirection: 'column', justifyContent: 'center', display: 'flex'}}>
+                        <p style={{color: "#000000", fontSize: '22px'}}>Este é o final da pesquisa. Muito obrigado pela colaboração!</p>
+                        {this.state.redirect_start ? (<Redirect to={{ pathname:"/" }}/>) : <div></div>}
+                    </center>
+                    {this.state.redirect ? (<Redirect to={{pathname:"/admin/classDashboard_" + participant_info.dashboard_sequence[0] + "/7/345/32"}}/>) : (<div></div>)}
+                </div>
+            ) : (
+                <Redirect to={{ pathname:"/", state: {message: 'Perdão, houve um problema durante a confirmação do termo de consentimento.'}}}/>
+            )
         )
     }
 
